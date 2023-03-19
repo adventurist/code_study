93e7998e536ad10e82a7075e3e776c86683201ed
logicp <logicp@logicp.ca>
2023-03-18 20:56:27 -0400

    cleanedup

--- long_promise/main.cpp
+++ long_promise/main.cpp
@@ -4,92 +4,110 @@
 #include <mutex>
 #include <condition_variable>
 
+template<typename ...Args>
+void log(Args... args) { for (auto arg : { args... }) std::cout << arg; std::cout << std::endl; };
 auto sleep = [] { std::this_thread::sleep_for(std::chrono::seconds(1)); };
 class application
 {
 private:
-std::mutex              m;
-std::condition_variable cv;
-bool                    f1_done = false;
-bool                    f2_done = false;
-bool                    handled = false;
-bool                    waiting = false;
-std::future<void>       f;
+  std::future<void>       f;
+  std::mutex              m;
+  std::condition_variable cv;
+  std::thread             t1;
+  std::thread             t2;
+  std::thread             t3;
+  std::thread             t4;
+  bool                    f1_done = false;
+  bool                    f2_done = false;
+  bool                    handled = false;
+  bool                    waiting = false;
 
-//--------------------------------------
-void f2(std::promise<void>&& p)
-{
-  std::cout << "f2" << std::endl;
+  //--------------------------------------
+  void f2(std::promise<void>&& p) // work function 2
+  {
+    log("f2()          ::thread 2");
+    t4 = std::thread{[this](std::promise<void>&& p)
     {
-      sleep();
-      std::unique_lock lock{m};
-      cv.wait(lock, [this] { return waiting; });
-      f2_done = true;
-    }
+      {
+        sleep();
+        log("f2()          ::thread 4    => Waiting for wait_promise::thread 3 to set promise to wait");
+        std::unique_lock lock{m};
+        cv.wait(lock, [this] { return waiting; });
+        f2_done = true;
+        log("f2()          ::thread 4    => Setting value");
+        cv.notify_one();
+        p.set_value();
+      }
+    }, std::move(p)};
+  }
+  //--------------------------------------
+  void f1(std::promise<void>&& p) // work function 1
+  {
+    log("f1()          ::thread 1");
+    t2 = std::thread{[this] (auto&& p) { wait_promise(std::move(p)); } , std::move(p)};
 
-    std::cout << "Setting value" << std::endl;
+    {
+      log("f1()          ::thread 2    => setting f1 done");
+      std::unique_lock lock{m};
+      f1_done = true;
+    }
     cv.notify_one();
-    p.set_value();
-}
-//--------------------------------------
-void f1(std::promise<void>&& p)
-{
-  std::cout << "f1" << std::endl;
-  std::thread{[this] (auto&& p) { wait_promise(std::move(p)); } , std::move(p)}.detach();
-
-  {
-    std::unique_lock lock{m};
-    f1_done = true;
   }
-  cv.notify_one();
-}
-//--------------------------------------
-void wait_promise(std::promise<void>&& p)
-{
-  std::cout << "wait promise" << std::endl;
-  std::thread{[this] {
+  //--------------------------------------
+  void wait_promise(std::promise<void>&& p)
+  {
+    log("wait_promise()::thread 2");
+    t3 = std::thread{[this]
+    {
+      {
+        log("wait_promise()::thread 3    => Waiting for f1()::thread 2 to complete");
+        std::unique_lock lock{m};
+        sleep();
+        waiting = true;
+        cv.wait(lock, [this] { return f1_done; });
+      }
+    }};
+    log("wait_promise()::thread 2    => sending promise to f2()");
+    f2(std::move(p));
+    cv.notify_one();
+  }
+  //--------------------------------------
+  void handle(std::future<void>& f)
+  {
+    log("handle()      ::main thread => Waiting for future to become ready");
     {
       std::unique_lock lock{m};
-      sleep();
-      waiting = true;
-      cv.wait(lock, [this] { return f1_done; });
+      cv.wait(lock, [this, &f]
+      {
+        bool ready = (f1_done && f2_done);
+        return ready && f.valid() && f.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+      });
+      handled = true;
     }
-  }}.detach();
-
-  f2(std::move(p));
-  cv.notify_one();
-}
-//--------------------------------------
-void handle(std::future<void>& f)
-{
-  std::cout << "handle" << std::endl;
-  {
-    std::unique_lock lock{m};
-    cv.wait(lock, [this, &f]
-    {
-      bool ready = (f1_done && f2_done);
-      return ready && f.valid() && f.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
-    });
-    handled = true;
+    log("handle()      ::main thread => Getting future");
+    f.get();
   }
-  f.get();
-}
 
 public:
-  application(std::promise<void>&& p)
+  //--------------------------------------
+  application(std::promise<void>&& p) // ctor
   {
     f = p.get_future();
-    std::thread{[this, &p] { f1(std::move(p)); }}.detach();
+    t1 = std::thread{[this, &p] { f1(std::move(p)); }};
     handle(f);
   }
-
-  bool promise_is_handled() const
+  //--------------------------------------
+  ~application()                      // dtor
   {
-    return handled;
+    if (t1.joinable()) t1.join();
+    if (t2.joinable()) t2.join();
+    if (t3.joinable()) t3.join();
+    if (t4.joinable()) t4.join();
   }
+}; // application
 
-};
 //----------------------------------------------------------------------------
+//--MAIN----------------------------------------------------------------------
 //----------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
